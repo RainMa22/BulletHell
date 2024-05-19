59fec4ed0ad9caf0af5f7ab271452846a01245a5
Rainma22 <d2rain@126.com>
2024-05-18 21:12:27 -0700

    Reordered gamelogic

--- scenes/GameOver.gd
+++ scenes/GameOver.gd
@@ -6,14 +6,16 @@ class_name GameOver extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var boss_beaten: int = Global.gameover_stats.get("boss_beaten",0);
-	var personal_best: int = Global.get_peanut_butter();
-	var personal_best_msg: String = "";
 	var fade_from_texture: Texture2D =  Global.gameover_stats.get("last_frame_texture")
 	if(fade_from_texture!= null):
 		var melt: Melt = Melt.new();
 		melt.set_texture(fade_from_texture);
 		add_child(melt)
+		
+	var boss_beaten: int = Global.gameover_stats.get("boss_beaten",0);
+	var personal_best: int = Global.get_peanut_butter();
+	var personal_best_msg: String = "";
+	
 	if boss_beaten > personal_best:
 		personal_best_msg = "New Personal Best!"
 		personal_best = boss_beaten
